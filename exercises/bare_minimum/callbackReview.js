582f70e7ba1899d74f29f13da68ee17f9fc65a31
zz00024 <zz00024@gmail.com>
2019-09-24 17:33:21 -0700

    firstReview completed

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -19,8 +19,15 @@ var pluckFirstLineFromFile = function (filePath, callback) {
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request(url, (err, res) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
